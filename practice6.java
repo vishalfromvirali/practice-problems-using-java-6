5f8d2aa5d455511f6d2e4621f01562e5530517f0
vishal p <vishalfromvirali@gmail.com>
2024-12-14 00:39:45 +0530

    Add files via upload

---
+++ practice6.java
@@ -0,0 +1,19 @@
+
+class practice6{
+    public static void main(String[]args){
+        int[] num={1,2,4,5,0,0};
+        int[] num1={1,2,2,0,0,5};
+
+        
+        int n=0;
+        for(int i=0;i<num.length;i++){
+            if(num[0]!=num1[i]){
+                n=num[i];
+
+            }
+            
+        
+    }
+    System.out.println(n);
+}
+}
